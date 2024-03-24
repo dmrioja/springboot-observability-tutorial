bada023d94fd9f4b0db923304d73cf34a1c765b4
dmunoz <d.mrioja@gmail.com>
2024-03-24 13:05:03 +0100

    05: add Grafana Loki

--- src/main/java/com/observability/poc/serviceapp/ServiceAppApplication.java
+++ src/main/java/com/observability/poc/serviceapp/ServiceAppApplication.java
@@ -1,5 +1,7 @@
 package com.observability.poc.serviceapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -25,6 +28,7 @@ public class ServiceAppApplication {
 	@RestController
 	class HelloController {
 
+		private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 		private final RestTemplate restTemplate;
 
 		HelloController(RestTemplate restTemplate) {
@@ -33,9 +37,22 @@ public class ServiceAppApplication {
 
 		@GetMapping("/hello")
 		public String hello() {
+			LOGGER.info("---------Hello method started---------");
+			LOGGER.error("---------Hello method started, id missing!---------");
 			ResponseEntity<String> responseEntity = this.restTemplate.postForEntity(
 				"https://httpbin.org/post", "Hello Cloud!", String.class);
 			return responseEntity.getBody();
+		}
+
+		@GetMapping("/exception")
+		public String exception() {
+			throw new IllegalArgumentException("This id is invalid");
+		}
+
+		@ExceptionHandler(value = { IllegalArgumentException.class })
+		protected ResponseEntity<String> handleConflict(IllegalArgumentException ex) {
+			LOGGER.error(ex.getMessage(), ex);
+			return ResponseEntity.badRequest().body(ex.getMessage());
 		}
 		
 	}
